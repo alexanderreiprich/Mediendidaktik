43a1bf1e9195d59ee65dcb127901c340a0d5fa0f
Julius Engelhardt <julius.engelhardt@stud.htwk-leipzig.de>
2025-07-07 18:13:33 +0200

    Added disabled Buttons

--- concept_app/src/components/UI/TaskCard.jsx
+++ concept_app/src/components/UI/TaskCard.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './TaskCard.css';
 import Button from '../UI/Button';
 
-const TaskCard = ({children}) => {
+const TaskCard = ({children, buttonMode = 0}) => {
+  const [firstButton, setFirstButton] = useState("disabled");
+  const [secondButton, setSecondButton] = useState("disabled");
+
+  useEffect(() => {
+    if (buttonMode >= 1) {
+      setFirstButton("")
+    }
+    if (buttonMode >= 2) {
+      setSecondButton("")
+    }
+  }, [buttonMode]);
+
   return (
     <div className="task-card">
       <span className='title'>{children}</span>
       <div className='button-container'>
-        <Button>Zur Aufgabe</Button>
-        <Button>Musterlösung</Button>
+        <Button className={firstButton}>Zur Aufgabe</Button>
+        <Button className={secondButton}>Musterlösung</Button>
       </div>
     </div>
   );
